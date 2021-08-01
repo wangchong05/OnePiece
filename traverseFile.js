6364a4f07459afba8b8243a60625d7a4849d0b9f
wangchong05 <wangchong46aice@163.com>
2021-08-02 00:03:11 +0800

    add traverseFile.js

--- traverseFile.js
+++ traverseFile.js
@@ -2,8 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
 
-
-var filePath = path.resolve('./');//当前文件夹
+var filePath = path.resolve('./');
 
 //调用文件遍历方法
 fileDisplay(filePath);
@@ -13,51 +12,32 @@ fileDisplay(filePath);
  * @param filePath 需要遍历的文件路径
  */
 function fileDisplay(filePath){
-    //根据文件路径读取文件，返回文件列表
     fs.readdir(filePath, function(err, files){
         if(err){
             console.warn(err)
         }else{
             //遍历读取到的文件列表
             files.forEach(function(filename){
-                //获取当前文件的绝对路径
-                var filedir = path.join(filePath, filename);
+                var filedir = path.join(filePath, filename);//获取当前文件的绝对路径
                 //根据文件路径获取文件信息，返回一个fs.Stats对象
                 fs.stat(filedir, function(eror, stats){
                     if(eror){
                         console.warn('Failed to get the file stats');
                     }else{
-                        var isFile = stats.isFile();//是文件
-                        var isDir = stats.isDirectory();//是文件夹
+                        var isFile = stats.isFile();
+                        var isDir = stats.isDirectory();
                         if(isFile){
-                            if(filedir.indexOf('\\asd\\') != -1) {// asd文件夹下的文件
-                              // fs.readFile(filedir, function(err, data) {
-                              //   if (err) return err;
-                              //   let content = data.toString();
-                              //   content = content.replace('replaceTextName', 'hyachyac');
-                              //   fs.writeFileSync(filedir, content, function(err) {
-                              //     if (err) return err;
-                              //   })
-                              //   // readline.insert(filedir, 2, 'hello');
-
-
-
-                              // })
-                              
+                            if(filedir.indexOf('stories.tsx') != -1) {// stories.tsx后缀的文件
 
                               var fRead = fs.createReadStream(filedir); 
-                              // var fWrite = fs.createWriteStream(filedir);
-                              var fWrite = fs.createWriteStream('output.txt');
 
 
                               var objReadline = readline.createInterface({ 
-                                input: fRead,
-                                output: fWrite
+                                input: fRead
                               }); 
 
                               objReadline.on('line', (line)=>{
-                                if(line.indexOf('hyac') != -1) {
-                                  // fWrite.write('strTemp'); 
+                                if(line.indexOf("from '..") != -1) {// 判断 "from '.." 
                                   line = '789789'
                                   fs.writeFileSync(filedir, line, function(err) {
                                     if (err) return err;
